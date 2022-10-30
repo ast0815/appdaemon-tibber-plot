7ceef21c6129e74451c11f8115779b9b0a484f61
Lukas Koch <lukas.koch@mailbox.org>
2022-10-30 11:35:46 +0100

    Fix plotting of additional data when timezones are different.

--- tibber_price_plot.py
+++ tibber_price_plot.py
@@ -99,7 +99,7 @@ class TibberPricePlot(hass.Hass):
                     self.log(f"Could not load {varname} from global variables.")
                     continue
                 df = pd.DataFrame({"datetime": series.index, "value": series.array})
-                df["datetime"] = df["datetime"].dt.astimezone(tz)
+                df["datetime"] = df["datetime"].dt.tzconvert(tz)
                 df["date"] = df["datetime"].dt.date
                 df["time"] = df["datetime"].dt.hour + (df["datetime"].dt.minute / 60)
                 # Insert dummy values to plot the last hour
